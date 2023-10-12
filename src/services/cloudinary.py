4987b2379923e521998885d2e3cdf626e309e384
Heorhii <kaplitskiygv@gmail.com>
2023-10-12 15:08:17 +0300

    issues - PWEB-13: - cloudinary -> add cloudinary fiche ( optimization, upload, transformation) - change confi

--- src/services/cloudinary.py
+++ src/services/cloudinary.py
@@ -1,20 +1,31 @@
+"""
+Processing our images using the Cloudinary service
+"""
+
 from typing import BinaryIO
 
 import cloudinary
+
+# Import the cloudinary.api for managing assets
 from cloudinary.api import resource
+
+# Import the cloudinary.uploader for uploading assets
 from cloudinary.uploader import upload
 
-from config import settings
+# import config setting
+from config.config import settings
 
 cloudinary.config(
     cloud_name=settings.cloudinary_name,
     api_key=settings.cloudinary_api_key,
     api_secret=settings.cloudinary_api_secret,
-    secure=True
+    secure=True,
 )
 
 
-def upload_image(file: BinaryIO, user_id: int, return_size: tuple[int, int] = (250, 250)) -> str:
+def upload_image(
+    file: BinaryIO, user_id: int, return_size: tuple[int, int] = (250, 250)
+) -> str:
     """
     The upload_image function takes a file, uploads it to Cloudinary and returns the URL of the uploaded image.
 
@@ -23,9 +34,53 @@ def upload_image(file: BinaryIO, user_id: int, return_size: tuple[int, int] = (2
     :param return_size: tuple[int, int]: Set the width and height of the image
     :return: The url of the uploaded image
     """
-    file_id = settings.cloudinary_folder + f'avatar_{user_id}'
+    file_id = settings.cloudinary_folder + f"avatar_{user_id}"
     upload(file, public_id=file_id, owerwrite=True)
 
     return cloudinary.CloudinaryImage(file_id).build_url(
-        width=return_size[0], height=return_size[1], crop='fill', version=resource(file_id)['version']
+        width=return_size[0],
+        height=return_size[1],
+        crop="fill",
+        version=resource(file_id)["version"],
     )
+
+
+def transform_img(public_id, width, height, crop):
+    """
+    The transform_img function takes a Cloudinary public_id and an optional quality parameter to optimize the image.
+
+    :param public_id: str: Public ID of the image to optimize
+    :param width: set the image width for cropping
+    :param height: set the image height for cropping
+    :param crop: a command that crops according to the Height and Width parameters we set
+    :return: The returns us images from Cloudinary transformed_url
+    """
+
+    # url, options = cloudinary_url("olympic_flag", width=100, height=150, crop="fill")
+    # Crete object CloudinaryImage
+    image = cloudinary.CloudinaryImage(public_id)
+
+    # Crete object Transformation
+    transformation = cloudinary.Transformation()
+    transformation.width(width)
+    transformation.height(height)
+    transformation.crop(crop)
+
+    # Get URL transform image изображения
+    transformed_url = image.build_url(transformation=transformation)
+
+    return transformed_url
+
+
+def optimize_media(public_id, quality=80):
+    """
+    The optimize_media function takes a Cloudinary public_id and an optional quality parameter to optimize the image.
+
+    :param public_id: str: Public ID of the image to optimize
+    :param quality: int: Image quality (0-100), where 0 is the lowest quality and 100 is the highest.
+    :return: The response from Cloudinary
+    """
+    # Set quality for optimization
+    update_result = cloudinary.update(public_id, quality=quality)
+
+    return update_result
