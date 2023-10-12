01d740d2e6456169965f1333613f6efd1a259b00
Noma9d <woody0740@gmail.com>
2023-10-12 22:52:07 +0300

    modified:   src/repository/image.py 	modified:   src/routes/image.py 	modified:   src/services/auth.py

--- src/repository/image.py
+++ src/repository/image.py
@@ -6,32 +6,65 @@ import shutil
 from sqlalchemy import and_
 
 
-async def create_image(file, user_id: int, description: str, tags: List, db: Session):
+def create_image(file, user_id: int, description: str, tags: List[str], db: Session) -> Photo:
+    """
+    Создает изображение, сохраняет его на сервере и связывает его с тегами.
+
+    Parameters:
+    - file: загружаемый файл изображения.
+    - user_id: идентификатор пользователя, загружающего изображение.
+    - description: описание изображения.
+    - tags: список тегов, связанных с изображением.
+    - db: объект сессии базы данных SQLAlchemy.
+
+    Returns:
+    - Photo: объект Photo, представляющий созданное изображение.
+    """
+    # Создание объекта Photo
     image = Photo(user_id, description=description)
     db.add(image)
     db.commit()
     db.refresh(image)
 
+    # Перебор тегов и создание или получение из базы данных
     for tag_name in tags:
         try:
+            # Попытка найти тег в базе данных
             tag = db.query(Tag).filter(Tag.name == tag_name).one()
         except NoResultFound:
+            # Создание нового тега, если не найден
             tag = Tag(name=tag_name)
             db.add(tag)
             db.commit()
 
+        # Присоединение тега к изображению
         image.tags.append(tag)
 
+    # Сохранение файла изображения на сервере
     with open(f"images/{image.id}_{file.filename}", "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
 
+    # Обновление пути к файлу изображения в объекте Photo
     image.path = f"images/{image.id}_{file.filename}"
     db.commit()
 
+    # Возвращение объекта Photo
     return image
 
 
+
 async def get_image(image_id: int, user_id: int, db: Session):
+    """
+    Получает изображение по его идентификатору и идентификатору пользователя.
+
+    Parameters:
+    - image_id: идентификатор изображения.
+    - user_id: идентификатор пользователя.
+    - db: объект сессии базы данных SQLAlchemy.
+
+    Returns:
+    - Photo: объект Photo, представляющий запрашиваемое изображение, или None, если изображение не найдено.
+    """
     return (
         db.query(Photo)
         .filter(and_(Photo.id == image_id, Photo.user_id == user_id))
@@ -42,11 +75,26 @@ async def get_image(image_id: int, user_id: int, db: Session):
 async def change_description(
     image_id: int, user_id: int, description: str, db: Session
 ):
+    """
+    Изменяет описание изображения.
+
+    Parameters:
+    - image_id: идентификатор изображения.
+    - user_id: идентификатор пользователя.
+    - description: новое описание изображения.
+    - db: объект сессии базы данных SQLAlchemy.
+
+    Returns:
+    - Optional[Photo]: объект Photo, представляющий измененное изображение, или None, если изображение не найдено.
+    """
+    # Поиск изображения по идентификаторам
     image = (
         db.query(Photo)
         .filter(and_(Photo.id == image_id, Photo.user_id == user_id))
         .first()
     )
+    
+    # Если изображение найдено, изменение описания и сохранение изменений в базе данных
     if image:
         image.description = description
         db.commit()
@@ -54,12 +102,27 @@ async def change_description(
     return image
 
 
-async def delte_image_by_id(image_id: int, user_id: int, db: Session):
+
+async def delte_image_by_id(image_id: int, user_id: int, db: Session)->bool:
+    """
+    Удаляет изображение по его идентификатору и идентификатору пользователя.
+
+    Parameters:
+    - image_id: идентификатор изображения.
+    - user_id: идентификатор пользователя.
+    - db: объект сессии базы данных SQLAlchemy.
+
+    Returns:
+    - bool: True, если изображение успешно удалено, иначе False.
+    """
+    # Поиск и удаление изображения из базы данных
     image = (
         db.query(Photo)
         .filter(and_(Photo.id == image_id, Photo.user_id == user_id))
         .delete()
     )
+
+    # Сохранение изменений в базе данных
     if image:
         db.commit()
 
--- src/routes/image.py
+++ src/routes/image.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Depends, status, UploadFile, File, Form, HTTPException
+from starlette.status import HTTP_500_INTERNAL_SERVER_ERROR, HTTP_404_NOT_FOUND
 from sqlalchemy.orm import Session
 from typing import List
 from src.database.models import Photo, Tag, User
@@ -13,15 +14,29 @@ router = APIRouter(prefix="/images", tags=["images"])
 @router.post("/uploadimage/", response_model=ImageModel)
 async def create_image(
     file: UploadFile = File(),
-    description: str = Form,
+    description: str = Form(),
     tags: List[str] = Form([]),
     db: Session = Depends(get_db),
-    current_user: User = Depends(auth_service.get_current_user),
-):
+    current_user: User = Depends(auth_service.get_current_user_as_admin),
+) -> ImageModel:
+    """
+    Эндпоинт для загрузки изображения пользователем.
+
+    Parameters:
+    - file: файл изображения, загружаемый пользователем.
+    - description: строка, описывающая изображение.
+    - tags: список строк, представляющих теги, связанные с изображением.
+    - db: объект сессии базы данных SQLAlchemy.
+    - current_user: объект пользователя, представляющий текущего аутентифицированного пользователя.
+
+    Returns:
+    - ImageModel: модель изображения с полями id, user_id, description и tags.
+
+    """
+    # Попытка создать изображение
     image = await repository_image.create_image(
         file, description, tags, db, user_id=current_user.id
     )
-
     return {
         "id": image.id,
         "user_id": image.user_id,
@@ -33,9 +48,23 @@ async def create_image(
 @router.get("/getimage/{image_id}", response_model=ImageModel)
 async def get_image(
     image_id: int,
-    current_user: User = Depends(auth_service.get_current_user),
+    current_user: User = Depends(auth_service.get_current_user_as_admin),
     db: Session = Depends(get_db),
-):
+)->ImageModel:
+    """
+    Эндпоинт для получения изображения по его идентификатору.
+
+    Parameters:
+    - image_id: идентификатор изображения.
+    - current_user: объект пользователя, представляющий текущего аутентифицированного пользователя.
+    - db: объект сессии базы данных SQLAlchemy.
+
+    Returns:
+    - ImageModel: модель изображения с полями path, user_id, description и tags.
+
+    Raises:
+    - HTTPException: 404 Not Found, если изображение не найдено.
+    """
     image = await repository_image.get_image(image_id, db, user_id=current_user.id)
     if not image:
         raise HTTPException(
@@ -54,9 +83,24 @@ async def get_image(
 async def update_image(
     image_id: int,
     description: str = Form(),
-    current_user: User = Depends(auth_service.get_current_user),
+    current_user: User = Depends(auth_service.get_current_user_as_admin),
     db: Session = Depends(get_db),
-):
+)->dict:
+    """
+    Эндпоинт для обновления описания изображения.
+
+    Parameters:
+    - image_id: идентификатор изображения.
+    - description: новое описание изображения.
+    - current_user: объект пользователя, представляющий текущего аутентифицированного пользователя.
+    - db: объект сессии базы данных SQLAlchemy.
+
+    Returns:
+    - dict: словарь с сообщением об успешном обновлении.
+
+    Raises:
+    - HTTPException: 404 Not Found, если изображение не найдено.
+    """
     image = await repository_image.get_image(image_id, db, user_id=current_user.id)
     if not image:
         raise HTTPException(
@@ -72,8 +116,22 @@ async def update_image(
 async def delete_image(
     image_id: int,
     db: Session(get_db),
-    current_user: User = Depends(auth_service.get_current_user),
-):
+    current_user: User = Depends(auth_service.get_current_user_as_admin),
+)->dict:
+    """
+    Эндпоинт для удаления изображения по его идентификатору.
+
+    Parameters:
+    - image_id: идентификатор изображения.
+    - db: объект сессии базы данных SQLAlchemy.
+    - current_user: объект пользователя, представляющий текущего аутентифицированного пользователя.
+
+    Returns:
+    - dict: словарь с сообщением об успешном удалении.
+
+    Raises:
+    - HTTPException: 404 Not Found, если изображение не найдено.
+    """
     image = await repository_image.get_image(image_id, db, user_id=current_user.id)
     if not image:
         raise HTTPException(
